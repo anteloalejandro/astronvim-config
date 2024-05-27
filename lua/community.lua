6fe2222c099692b6969674015a4d28857862c9f4
Alejandro Antelo <alfasant@protonmail.com>
2024-05-27 18:29:42 +0200

    trimmed fat and orginized packages

--- lua/community.lua
+++ lua/community.lua
@@ -1,22 +1,20 @@
--- AstroCommunity: import any community modules here
--- We import this file in `lazy_setup.lua` before the `plugins/` folder.
--- This guarantees that the specs are processed before any user plugins.
-
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
+  -- colorscheme
   { import = "astrocommunity.colorscheme.gruvbox-nvim" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.astro" },
-  { import = "astrocommunity.pack.angular" },
-  { import = "astrocommunity.pack.php" },
-  { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.pack.html-css" },
-  { import = "astrocommunity.pack.python" },
+
+  -- language support
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.java" },
-  { import = "astrocommunity.pack.full-dadbod" },
   { import = "astrocommunity.pack.godot" },
+
+  -- sql tools
+  { import = "astrocommunity.pack.full-dadbod" },
 }
--- import/override with your plugins folder
