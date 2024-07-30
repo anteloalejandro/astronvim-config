14beef86ab23d033b145936786a4b41b04c598e0
Alejandro Antelo <alfasant@protonmail.com>
2024-07-30 22:03:13 +0200

    remove unused config function

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,10 +1,6 @@
 ---@type LazySpec
 return {
   {
-    "williamboman/mason-lspconfig.nvim",
-    opts = function(_, opts) opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {}) end,
-  },
-  {
     "jay-babu/mason-nvim-dap.nvim",
     opts = function(_, opts)
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
