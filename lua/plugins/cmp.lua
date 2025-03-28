afe5f09079f3d8f5a68e835c2d8aac262452f76a
Alejandro Antelo <alfasant@protonmail.com>
2025-03-28 13:04:21 +0100

    remove commented-out old code in cmp.lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -33,7 +33,7 @@ return {
         'fallback',
       },
       -- Navigate to the previous suggestion or cancel completion if currently on the first one.
-      ['<S-Tab>'] = { 
+      ['<S-Tab>'] = {
         function(cmp)
           if cmp.is_menu_visible() then
             return cmp.select_prev()
@@ -42,35 +42,5 @@ return {
         'fallback',
       },
     },
-    -- completion = {
-    --   menu = { enabled = true },
-    --   list = { selection = { preselect = false }, cycle = { from_top = false } },
-    -- }
-
-    -- -- NOTE: Originally found somewhere on the astronvim v3 docs
-    -- local function has_words_before()
-    --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-    -- end
-    --
-    -- -- Enable completion ignoring luasnip jumps
-    -- opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_next_item()
-    --   elseif has_words_before() then
-    --     cmp.complete()
-    --   else
-    --     fallback()
-    --   end
-    -- end)
-    --
-    -- -- The same as before, but does not trigger the cmp window
-    -- opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_prev_item()
-    --   else
-    --     fallback()
-    --   end
-    -- end)
   }
 }
